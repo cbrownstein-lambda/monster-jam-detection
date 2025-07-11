33e3e779cd8aa5ceaf8c4ead19ec62c7d9fca95e
Cody Brownstein <cody@lambdal.com>
2025-07-10 20:43:41 -0700

    Add ability to configure using command line arguments

--- download_data.py
+++ download_data.py
@@ -1,24 +1,24 @@
 import os
+import argparse
 from roboflow import Roboflow
 
+parser = argparse.ArgumentParser(description="Download a dataset from Roboflow.")
+parser.add_argument("--api_key", type=str, default=None, help="Roboflow API key")
+parser.add_argument("--workspace", type=str, default=None, help="Roboflow workspace name")
+parser.add_argument("--project", type=str, default=None, help="Roboflow project name")
+parser.add_argument("--version_number", type=int, default=None, help="Project version number")
+parser.add_argument("--download_format", type=str, default=None, help="Format to download (e.g., yolov8)")
+args = parser.parse_args()
+
 # --- Configuration Variables ---
-# Roboflow API key (should be set as an environment variable for security)
-API_KEY = os.environ.get("ROBOFLOW_API_KEY")
+API_KEY = args.api_key or os.environ.get("ROBOFLOW_API_KEY")
 if not API_KEY:
-    raise ValueError("ROBOFLOW_API_KEY environment variable not set.")
+    raise ValueError("ROBOFLOW_API_KEY must be provided as an argument or environment variable.")
 
-# Roboflow workspace name
-WORKSPACE = "cody-brownstein"
-
-# Roboflow project name
-PROJECT = "monster-jam-detection"
-
-# Project version number
-# See the latest versions: https://universe.roboflow.com/cody-brownstein/monster-jam-detection
-VERSION_NUMBER = 9
-
-# Format to download (e.g., "yolov8")
-DOWNLOAD_FORMAT = "yolov8"
+WORKSPACE = args.workspace or os.environ.get("ROBOFLOW_WORKSPACE", "cody-brownstein")
+PROJECT = args.project or os.environ.get("ROBOFLOW_PROJECT", "monster-jam-detection")
+VERSION_NUMBER = args.version_number or int(os.environ.get("ROBOFLOW_VERSION_NUMBER", 9))
+DOWNLOAD_FORMAT = args.download_format or os.environ.get("ROBOFLOW_DOWNLOAD_FORMAT", "yolov8")
 # --- End Configuration Variables ---
 
 rf = Roboflow(api_key=API_KEY)
